198420b2dec5bf750a4668266eec7cb2a2478659
Manu Järvinen <manu.jarvinen@gmail.com>
2018-09-05 05:50:45 +0300

    Update Assign_Logic_Bricks_to_selected_objects.py

--- Assign_Logic_Bricks_to_selected_objects.py
+++ Assign_Logic_Bricks_to_selected_objects.py
@@ -1,16 +1,19 @@
-# Note that all of the selected objects need to have at least one transform keyframe in them so that they have an action that can be assigned to the actuator logic brick, otherwise you have an error by running this.
+# This adds the logic bricks only to the selected objects that have an action. Actionless objects are ignored.
 
 import bpy
 
 selected = bpy.context.selected_objects
 
 for obj in selected:
-    bpy.context.scene.objects.active = obj
-    
-    bpy.ops.logic.sensor_add(type="ALWAYS")
-    bpy.ops.logic.controller_add(type="LOGIC_AND")
-    bpy.ops.logic.actuator_add(type="ACTION")
-    bpy.context.object.game.actuators['Action'].action=obj.animation_data.action
-    bpy.context.object.game.actuators['Action'].frame_end=9999
-    obj.game.sensors['Always'].link(obj.game.controllers['And']) 
-    obj.game.actuators['Action'].link(obj.game.controllers['And']) 
+    ad = obj.animation_data
+    if ad:
+        if ad.action:
+            bpy.context.scene.objects.active = obj
+            
+            bpy.ops.logic.sensor_add(type="ALWAYS")
+            bpy.ops.logic.controller_add(type="LOGIC_AND")
+            bpy.ops.logic.actuator_add(type="ACTION")
+            bpy.context.object.game.actuators['Action'].action=obj.animation_data.action
+            bpy.context.object.game.actuators['Action'].frame_end=9999
+            obj.game.sensors['Always'].link(obj.game.controllers['And']) 
+            obj.game.actuators['Action'].link(obj.game.controllers['And'])
