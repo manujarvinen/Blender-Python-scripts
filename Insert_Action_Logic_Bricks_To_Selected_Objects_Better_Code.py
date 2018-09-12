a842ce2730471b51df98425e206f2b3e6e5ec9eb
Manu Järvinen <manu.jarvinen@gmail.com>
2018-09-12 14:43:42 +0300

    SakariE new code 2018-09-12

--- Insert_Action_Logic_Bricks_To_Selected_Objects_Better_Code.py
+++ Insert_Action_Logic_Bricks_To_Selected_Objects_Better_Code.py
@@ -30,28 +30,19 @@ action_brick_end_frame = 9999
 # Credit goes mostly to Sakari Ekroth                       #
 #############################################################
 
-def findNoAction(selection):
-  noActionObjs = []
-  for obj in selection:
-    if not obj.animation_data or not obj.animation_data.action:
-        noActionObjs.append(obj.name)
-  if len(noActionObjs) > 0:
-    return True, noActionObjs
-  else:
-    return False, noActionObjs
+selection = bpy.context.selected_objects
+noActionObjs = []
+for obj in selection:
+  if not obj.animation_data or not obj.animation_data.action:
+    noActionObjs.append(obj.name)
 
-noActions, noActionObjs = findNoAction(bpy.context.selected_objects)
+if len(noActionObjs) > 0:
+  print ("Objects missing action: %s" % ", ".join(noActionObjs))
+  def oops(self, context):
+      self.layout.label("All of the selected objects must have an action. Cancelling... ")
 
-if noActions == False:
-    print("Inserting some Logic Bricks to the selected objects")
-
-if noActions:
-    print ("Objects missing action: %s" % ", ".join(noActionObjs))
-    def oops(self, context):
-        self.layout.label("All of the selected objects must have an action. Cancelling... ")
-
-    bpy.context.window_manager.popup_menu(oops, title="Error", icon='ERROR')
-    print("All of the selected objects must have an action. Cancelling... ")
+  bpy.context.window_manager.popup_menu(oops, title="Error", icon='ERROR')
+  print("All of the selected objects must have an action. Cancelling... ")
 
 ####################################################################
 # The following adds the Logic Bricks to objects that allow them to 
@@ -60,61 +51,58 @@ if noActions:
 # Credit goes mostly to Jeremy Behreandt
 ####################################################################
 
-if noActions == False:
-    ops=bpy.ops
-    context=bpy.context
+else:
+  print("Inserting some Logic Bricks to the selected objects")
+  ops = bpy.ops
+  context = bpy.context
 
-    # Cache shortcuts.
-    ops_mesh = ops.mesh
-    ops_object = ops.object
-    ops_logic = ops.logic
-    scene = context.scene
+  # Cache shortcuts.
+  ops_mesh = ops.mesh
+  ops_object = ops.object
+  ops_logic = ops.logic
+  scene = context.scene
 
-    selected = bpy.context.selected_objects
+  for obj in selection:
+    if obj.animation_data and obj.animation_data.action:
 
-    for obj in selected:
-        ad = obj.animation_data
-        if ad:
-            if ad.action:
-                bpy.context.scene.objects.active = obj
+      bpy.context.scene.objects.active = obj
+      current_object = context.active_object
+      # Cache shortcut to cube's game engine.
+      game_engine = current_object.game
 
-                current_object = context.active_object
+      # Add sensor.
+      ops_logic.sensor_add(type = "ALWAYS")
+                            #name='Always')
+                            #object=cube_name)
+      sensor = game_engine.sensors[-1]
 
-                # Cache shortcut to cube's game engine.
-                game_engine = current_object.game
+      # Add controller.
+      ops_logic.controller_add(type = "LOGIC_AND")
+                                #name='And')
+                                #object=cube_name)
+      controller = game_engine.controllers[-1]
 
-                # Add sensor.
-                ops_logic.sensor_add(type='ALWAYS')
-                                     #name='Always')
-                                     #object=cube_name)
-                sensor = game_engine.sensors[-1]
+      # Add actuator.
+      ops_logic.actuator_add(type = "ACTION")
+                              #name='Action',
+                              #object=cube_name)
+      actuator = game_engine.actuators[-1]
 
-                # Add controller.
-                ops_logic.controller_add(type='LOGIC_AND')
-                                         #name='And')
-                                         #object=cube_name)
-                controller = game_engine.controllers[-1]
-                
-                # Add actuator.
-                ops_logic.actuator_add(type='ACTION')
-                                       #name='Action',
-                                       #object=cube_name)
-                actuator = game_engine.actuators[-1]
+      # Set actuator action to cube's animation data action.
+      actuator.action = current_object.animation_data.action
 
-                # Set actuator action to cube's animation data action.
-                actuator.action = current_object.animation_data.action
+      # Play mode options are:
+      # ['PLAY', 'PINGPONG', 'FLIPPER', 'LOOPSTOP', 'LOOPEND', 'PROPERTY']
+      # 'PLAY' is the default.
+      #actuator.play_mode = play_mode
 
-                # Play mode options are:
-                # ['PLAY', 'PINGPONG', 'FLIPPER', 'LOOPSTOP', 'LOOPEND', 'PROPERTY']
-                # 'PLAY' is the default.
-                #actuator.play_mode = play_mode
+      # Set frame start to scene end.
+      actuator.frame_start = action_brick_start_frame
 
-                # Set frame start to scene end.
-                actuator.frame_start = action_brick_start_frame
+      # Set frame end to scene end.
+      actuator.frame_end = action_brick_end_frame
 
-                # Set frame end to scene end.
-                actuator.frame_end = action_brick_end_frame
-
-                # Link logic together.
-                sensor.link(controller)
-                actuator.link(controller)
+      # Link logic together.
+      sensor.link(controller)
+      actuator.link(controller)
+     
